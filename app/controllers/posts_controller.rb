c2865728b770c1d064204397d18eed8e87df89a1
afteralec <github email address>
2020-08-17 15:19:15 -0700

    Corrects syntax errors in posts_controller.rb

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,59 +1,58 @@
 class PostsController < ApplicationController
+  before_action :find_post, except: [:index, :new, :create]
 
-    class PostsController < ApplicationController
-        before_action :find_, except: [:index, :new, :create]
-    
-        def index
-            @posts = Post.all
-        end
-    
-        def show
-        end
-    
-        def new
-            @post = Post.new
-        end
-    
-        def edit
-        end
-    
-        def create
-            @post = Post.new(params[:])
-            if @post.save
-              flash[:success] = "Post successfully created"
-              redirect_to @post
-            else
-              flash[:error] = @post.errors.full_messages
-              render 'new'
-            end
-        end
-    
-        def update
-            if @post.update_attributes(params[:])
-              flash[:success] = "Post was successfully updated"
-              redirect_to @post
-            else
-              flash[:error] = @post.errors.full_messages
-              render 'edit'
-            end
-        end
-    
-        def destroy
-            if @post.destroy
-                flash[:success] = "Post was successfully deleted"
-                redirect_to @posts_path
-            else
-                flash[:error] = "Something went wrong"
-                redirect_to @posts_path
-            end
-        end
-    
-        private
-    
-            def find_
-                @post = Post.find(params[:id])
-            end
-    
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      flash[:success] = "Post successfully created"
+      redirect_to @post
+    else
+      flash[:error] = @post.errors.full_messages
+      render 'new'
     end
-    
+  end
+
+  def update
+      if @post.update_attributes(params[:])
+        flash[:success] = "Post was successfully updated"
+        redirect_to @post
+      else
+        flash[:error] = @post.errors.full_messages
+        render 'edit'
+      end
+  end
+
+  def destroy
+      if @post.destroy
+          flash[:success] = "Post was successfully deleted"
+          redirect_to @posts_path
+      else
+          flash[:error] = "Something went wrong"
+          redirect_to @posts_path
+      end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
 end
